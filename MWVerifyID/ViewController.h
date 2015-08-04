0540055b29abd7fda9e04eff1baa7a4c24b8f08e
WangTeng <woaijia_xxx@126.com>
2015-08-04 14:37:40 +0800

    init

---
+++ MWVerifyID/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MWVerifyID
+//
+//  Created by Tiny on 15/8/4.
+//  Copyright (c) 2015年 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MWVerifyID/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MWVerifyID
+//
+//  Created by Tiny on 15/8/4.
+//  Copyright (c) 2015年 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
